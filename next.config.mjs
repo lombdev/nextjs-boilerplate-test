bfc09854bff373d19941c56116f8232341e61f5c
LombDev <ruihorta88@hotmail.com>
2024-08-18 21:12:38 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ import './src/lib/env.mjs';
 
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+const nextConfig = { 
+  output: "standalone",
 };
 
 const withNextIntlConfig = withNextIntl('./src/i18n.ts');
